78e6efdc97c4229a084fc71eb0fbc828e32e3bc1
Siddiqui, Zohaib <zohaibms@outlook.com>
2022-03-03 14:44:03 -0600

    Routing

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const sequelize = require('./database');
-const User = require('./Users');
 
 sequelize.sync({ force: false }).then(() => console.log('db is ready'));
 
@@ -8,35 +7,8 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/users', async (req, res) => {
-  await User.create(req.body);
-  res.send("success");
-})
-
-app.get('/users', async (req, res) => {
-  const users = await User.findAll();
-  res.send(users);
-})
-
-app.get('/users/:id', async (req, res) => {
-  const id = req.params.id;
-  const user = await User.findOne({where: {id: id}});
-  res.send(user);
-})
-
-app.put('/users/:id', async (req, res) => {
-  const id = req.params.id;
-  const user = await User.findOne({where: {id: id}});
-  user.username = req.body.username;
-  await user.save();
-  res.send('updated');
-})
-
-app.delete('/users/:id', async (req, res) => {
-  const id = req.params.id;
-  await User.destroy({where: {id: id}});
-  res.send('removed');
-})
+/** Users Routings */
+require('./userRoutes')(app);
 
 app.listen(5000, () => {
   console.log("app is running");
---
+++ userRoutes.js
@@ -0,0 +1,43 @@
+module.exports = function(app){
+
+  const User = require('./Users');
+
+    app.post('/users', async (req, res) => {
+        await User.create(req.body);
+        res.send("success");
+      })
+      
+      app.get('/users', async (req, res) => {
+        const users = await User.findAll();
+        res.send(users);
+      })
+      
+      app.get('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const user = await User.findOne({where: {id: id}});
+        res.send(user);
+      })
+      
+      app.get('/users/:UserName/:Password',async (req, res)=> {
+        const username = req.params.UserName;
+        const pass = req.params.Password;
+        const user = await User.findOne({where: {UserName: username , Password: pass}});
+        res.send(user);
+      })
+      
+      app.put('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const user = await User.findOne({where: {id: id}});
+        user.username = req.body.username;
+        await user.save();
+        res.send('updated');
+      })
+      
+      app.delete('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        await User.destroy({where: {id: id}});
+        res.send('removed');
+      })
+
+    
+}
